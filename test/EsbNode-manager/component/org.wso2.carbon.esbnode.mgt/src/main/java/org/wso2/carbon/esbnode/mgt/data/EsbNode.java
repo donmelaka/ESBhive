461e39020f21d018e97af267d1654bab5d73764c
donmelaka <donmelaka@gmail.com>
2011-01-24 23:17:15 +0530

    added test folder with EsbNodeManager bundle

---
+++ test/EsbNode-manager/component/org.wso2.carbon.esbnode.mgt/src/main/java/org/wso2/carbon/esbnode/mgt/data/EsbNode.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Melaka
+ */
+package org.wso2.carbon.esbnode.mgt.data;
+
+public class EsbNode {
+	
+	private String IpAddress;
+	
+	public String getIpAddress(){
+		return this.IpAddress;
+	}
+	
+	public void setIpAddress(String IpAddress){
+		this.IpAddress = IpAddress;
+	}
+	
+}
+
