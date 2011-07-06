49dc4f86f3b99564e2a0af633b79ab6c1e3bf303
donmelaka <donmelaka@gmail.com>
2011-07-06 21:28:37 +0530

    cleaning up

--- node-manager/src/main/java/org/esbhive/node/mgt/ESBNode.java
+++ node-manager/src/main/java/org/esbhive/node/mgt/ESBNode.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.esbhive.node.mgt;
 
 import java.io.Serializable;
@@ -11,78 +10,72 @@ import java.io.Serializable;
  *
  * @author pubudu
  */
-public class ESBNode implements Serializable{
-  private String ipAndPort;
+public class ESBNode implements Serializable {
 
-  
-  private String ip;
-  private String httpsPort;
-  private String synapsePort;
-  private String username;
-  private String password;
+    private String ipAndPort;
+    private String ip;
+    private String httpsPort;
+    private String synapsePort;
+    private String username;
+    private String password;
 
-  public ESBNode(){
+    public ESBNode() {
+    }
 
-  }
+    public String getHttpsPort() {
+        return httpsPort;
+    }
 
-  public String getHttpsPort() {
-    return httpsPort;
-  }
+    public void setHttpsPort(String httpsPort) {
+        this.httpsPort = httpsPort;
+    }
 
-  public void setHttpsPort(String httpsPort) {
-    this.httpsPort = httpsPort;
-  }
+    public String getIp() {
+        return ip;
+    }
 
-  public String getIp() {
-    return ip;
-  }
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
 
-  public void setIp(String ip) {
-    this.ip = ip;
-  }
+    public String getSynapsePort() {
+        return synapsePort;
+    }
 
-  public String getSynapsePort() {
-    return synapsePort;
-  }
+    public void setSynapsePort(String synapsePort) {
+        this.synapsePort = synapsePort;
+    }
 
-  public void setSynapsePort(String synapsePort) {
-    this.synapsePort = synapsePort;
-  }
+    public ESBNode(String ip, String username, String password, String realIp, String httpsPort, String synapsePort) {
+        this.ipAndPort = ip;
+        this.username = username;
+        this.password = password;
+        this.ip = realIp;
+        this.httpsPort = httpsPort;
+        this.synapsePort = synapsePort;
+    }
 
+    public String getIpAndPort() {
+        return ipAndPort;
+    }
 
+    public void setIpAndPort(String ip) {
+        this.ipAndPort = ip;
+    }
 
-  public ESBNode(String ip, String username, String password,String realIp,String httpsPort,String synapsePort) {
-    this.ipAndPort = ip;
-    this.username = username;
-    this.password = password;
-    this.ip = realIp;
-    this.httpsPort = httpsPort;
-    this.synapsePort = synapsePort;
-  }
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public String getUsername() {
+        return username;
+    }
 
-  public String getIpAndPort() {
-    return ipAndPort;
-  }
-
-  public void setIpAndPort(String ip) {
-    this.ipAndPort = ip;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
