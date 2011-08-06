8c0056b7d4f69ef43fca8c3f25b0e5c2868c8270
pubudu <pubudu@ubuntu.(none)>
2011-08-06 15:40:04 +0530

    Data generation done. Have to do the graph drawing part.

--- demoapp/src/main/java/org/esbhive/demoapp/App.java
+++ demoapp/src/main/java/org/esbhive/demoapp/App.java
@@ -1,55 +1,72 @@
 package org.esbhive.demoapp;
 
 import java.io.File;
-import java.rmi.RemoteException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.CodeSource;
+import java.util.concurrent.CountDownLatch;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
-import samples.services.SimpleStockQuoteServiceCallbackHandler;
-import samples.services.SimpleStockQuoteServiceStub;
-import samples.services.xsd.GetQuote;
 
 /**
  * Hello world!
  *
  */
 public class App {
-  static String[] url_list= null;
+
+  static String[] url_list = null;
+
   public static void main(String[] args) {
-    String esb_home = null ;
+    String esb_home = null;
     int numReqestsPerClient = 0;
+    int numTotalClients = 0;
+    int numClientMachines = 0;
+    URL url = null;
+    CodeSource src = null;
     try {
-      Configuration config = new PropertiesConfiguration("options.properties");
+      src = App.class.getProtectionDomain().getCodeSource();
+      if (src != null) {
+        url = new URL(src.getLocation(), "options.properties");
+      }
+      Configuration config = new PropertiesConfiguration(url);
       esb_home = config.getString("esb.home");
       url_list = config.getStringArray("url.list");
+      numTotalClients = config.getInt("num.total.clients");
+      numClientMachines = config.getInt("num.client.machines");
       numReqestsPerClient = config.getInt("requests.perclient");
     } catch (ConfigurationException ex) {
       Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (MalformedURLException ex) {
+      Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    System.setProperty("javax.net.ssl.trustStore", esb_home + File.separator
+            + "resources" + File.separator + "security" + File.separator + "wso2carbon.jks");
+    System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
+
+    CountDownLatch doneSignal = new CountDownLatch(numTotalClients / numClientMachines * numReqestsPerClient);
+    File dir = null;
+    try {
+      String dirName = "numesbs-"+url_list.length+".totalclients-"+numTotalClients+
+              ".requestsperclient-"+numReqestsPerClient;
+      url = new URL(src.getLocation(), "data");
+      dir = new File(new File(url.toURI()), dirName);
+      System.out.println(dir.getAbsolutePath());
+      dir.mkdirs();
+    } catch (URISyntaxException ex) {
+      Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (MalformedURLException ex) {
+      Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    for (int i = 0; i < numTotalClients / numClientMachines; i++) {
+      new Client(numReqestsPerClient, doneSignal, dir ).start();
     }
     try {
-      System.setProperty("javax.net.ssl.trustStore", esb_home + File.separator + "resources" + File.separator + "security" + File.separator + "wso2carbon.jks");
-      System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
-      GetQuote gq = new GetQuote();
-      gq.setSymbol("IBM");
-      for (int i = 0; i < numReqestsPerClient; i++) {
-        Long curTime = System.nanoTime();
-        SimpleStockQuoteServiceCallbackHandler ssqscbh =
-                new SimpleStockQuoteServiceCallbackHandler(curTime) {
-
-                  @Override
-                  public void receiveResultgetQuote(samples.services.xsd.GetQuoteResponse result) {
-                    long time = System.nanoTime() - ((Long)this.clientData).longValue();
-                    System.out.println(time);
-                  }
-                };
-        int next = (int)Math.rint(Math.random() * (App.url_list.length-1));
-        SimpleStockQuoteServiceStub sqps = new SimpleStockQuoteServiceStub(App.url_list[next]+"/services/StockQuoteProxy.StockQuoteProxyHttpSoap12Endpoint");
-        sqps.startgetQuote(gq, ssqscbh);
-      }
-      System.console().readLine();
-    } catch (RemoteException ex) {
+      doneSignal.await();
+    } catch (InterruptedException ex) {
       Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
     }
   }
