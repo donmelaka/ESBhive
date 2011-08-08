13643417b5acaa06d9a7521b33146980ba8533e2
guest <guest@coolfyp-laptop.(none)>
2011-08-08 15:21:38 +0530

    Throughput chart working.

--- demoapp/src/main/java/org/esbhive/demoapp/ChartBuilder.java
+++ demoapp/src/main/java/org/esbhive/demoapp/ChartBuilder.java
@@ -27,6 +27,10 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.TreeMap;
 
 /**
  *
@@ -34,123 +38,136 @@ import java.util.HashMap;
  */
 public class ChartBuilder {
 
-  HashMap<Integer, HashMap<Integer, ArrayList<ResponseDataCalculator>>> dataMap =
-          new HashMap<Integer, HashMap<Integer, ArrayList<ResponseDataCalculator>>>();
+	HashMap<Integer, HashMap<Integer, ArrayList<ResponseDataCalculator>>> dataMap =
+		new HashMap<Integer, HashMap<Integer, ArrayList<ResponseDataCalculator>>>();
 
-  void addCalculatedDataItem(ResponseDataCalculator rdc) {
-    if (dataMap.containsKey(rdc.getRequestsPerClient())) {
-      HashMap<Integer, ArrayList<ResponseDataCalculator>> entry =
-              dataMap.get(rdc.getRequestsPerClient());
-      if (entry.containsKey(rdc.getNumESBs())) {
-        ArrayList list = entry.get(rdc.getNumESBs());
-        list.add(rdc);
-      } else {
-        ArrayList list = new ArrayList();
-        list.add(rdc);
-        entry.put(rdc.getNumESBs(), list);
-      }
-    } else {
-      HashMap<Integer, ArrayList<ResponseDataCalculator>> entry =
-              new HashMap<Integer, ArrayList<ResponseDataCalculator>>();
-      ArrayList list = new ArrayList();
-      list.add(rdc);
-      entry.put(rdc.getRequestsPerClient(), list);
-    }
-  }
+	void addCalculatedDataItem(ResponseDataCalculator rdc) {
+		if (dataMap.containsKey(rdc.getRequestsPerClient())) {
+			HashMap<Integer, ArrayList<ResponseDataCalculator>> entry =
+				dataMap.get(rdc.getRequestsPerClient());
+			if (entry.containsKey(rdc.getNumESBs())) {
+				ArrayList list = entry.get(rdc.getNumESBs());
+				list.add(rdc);
+			} else {
+				ArrayList list = new ArrayList();
+				list.add(rdc);
+				entry.put(rdc.getNumESBs(), list);
+			}
+		} else {
+			HashMap<Integer, ArrayList<ResponseDataCalculator>> entry =
+				new HashMap<Integer, ArrayList<ResponseDataCalculator>>();
+			ArrayList list = new ArrayList();
+			list.add(rdc);
+			entry.put(rdc.getNumESBs(), list);
+			dataMap.put(rdc.getRequestsPerClient(), entry);
+		}
+	}
 
-  public void createChart() throws MalformedURLException, IOException{
-    saveChart(createChartString(),"Test.png");
-  }
+	public void createChart() throws MalformedURLException, IOException {
+		for (Iterator<Integer> iter = dataMap.keySet().iterator(); iter.hasNext();) {
+			Integer requestPerClient = iter.next();
+			saveChart(createThroughputChartString(requestPerClient, dataMap.get(requestPerClient)),
+				"Requests-" + requestPerClient + "-Throughput.png");
+		}
+	}
 
-  private String createChartString() {
-// EXAMPLE CODE START
+	private String createThroughputChartString(Integer requestPerClient, HashMap<Integer, ArrayList<ResponseDataCalculator>> chartData) {
+		// Defining lines
+		List<Line> lines = new ArrayList<Line>();
+		List<String> xAxisValues = new ArrayList<String>();
+		boolean firstRun = true;
+		for (Iterator<Integer> iter = chartData.keySet().iterator(); iter.hasNext();) {
+			Integer numESBs = iter.next();
+			ArrayList<ResponseDataCalculator> lineData = chartData.get(numESBs);
+			TreeMap<Integer, Double> linePoints = new TreeMap<Integer, Double>();
+			for (Iterator<ResponseDataCalculator> rdcIter = lineData.iterator(); rdcIter.hasNext();) {
+				ResponseDataCalculator rdc = rdcIter.next();
+				linePoints.put(rdc.getNumClients(), (double) rdc.getThroughput());
 
-    // Defining lines
-    final int NUM_POINTS = 25;
-    final double[] competition = new double[NUM_POINTS];
-    final double[] mywebsite = new double[NUM_POINTS];
-    for (int i = 0; i < NUM_POINTS; i++) {
-      competition[i] = 100 - (Math.cos(30 * i * Math.PI / 180) * 10 + 50) * i / 20;
-      mywebsite[i] = (Math.cos(30 * i * Math.PI / 180) * 10 + 50) * i / 20;
-    }
-    Line line1 = Plots.newLine(Data.newData(mywebsite), Color.newColor("CA3D05"), "My Website.com");
-    line1.setLineStyle(LineStyle.newLineStyle(3, 1, 0));
-    line1.addShapeMarkers(Shape.DIAMOND, Color.newColor("CA3D05"), 12);
-    line1.addShapeMarkers(Shape.DIAMOND, Color.WHITE, 8);
-    Line line2 = Plots.newLine(Data.newData(competition), Color.SKYBLUE, "Competition.com");
-    line2.setLineStyle(LineStyle.newLineStyle(3, 1, 0));
-    line2.addShapeMarkers(Shape.DIAMOND, Color.SKYBLUE, 12);
-    line2.addShapeMarkers(Shape.DIAMOND, Color.WHITE, 8);
+			}
+			double[] points = new double[linePoints.values().size()];
+			int i = 0;
+			for (Iterator<Double> pointIter = linePoints.values().iterator(); pointIter.hasNext();) {
+				points[i] = pointIter.next().doubleValue();
+				i++;
+			}
+			for (Iterator xAxisValIter = linePoints.keySet().iterator(); xAxisValIter.hasNext() && firstRun;) {
+				xAxisValues.add(xAxisValIter.next().toString());
+			}
+			firstRun = false;
+			Random numGen = new Random();
+			Line line = Plots.newLine(Data.newData(points),
+				Color.newColor("" + numGen.nextInt(10) + "" + numGen.nextInt(10) + "" + numGen.nextInt(10) + "" + numGen.nextInt(10) + "" + numGen.nextInt(10) + "" + numGen.nextInt(10)),
+				"For " + numESBs + " ESB(s)");
+			line.setLineStyle(LineStyle.newLineStyle(3, 1, 0));
+			line.addShapeMarkers(Shape.DIAMOND, Color.newColor("CA3D05"), 12);
+			line.addShapeMarkers(Shape.DIAMOND, Color.WHITE, 8);
+			lines.add(line);
+
+		}
+
+		LineChart chart = GCharts.newLineChart(lines);
+		chart.setSize(600, 450);
+		chart.setTitle("Throughput with " + requestPerClient + " requests per client.", Color.WHITE, 14);
+		chart.setGrid(1, 25, 3, 2);
+
+		AxisStyle axisStyle = AxisStyle.newAxisStyle(Color.WHITE, 12, AxisTextAlignment.CENTER);
+		AxisLabels xAxis = AxisLabelsFactory.newAxisLabels(xAxisValues);
+		xAxis.setAxisStyle(axisStyle);
+		AxisLabels yAxis = AxisLabelsFactory.newAxisLabels("", "25", "50", "75", "100");
+		yAxis.setAxisStyle(axisStyle);
+		AxisLabels yAxis2 = AxisLabelsFactory.newAxisLabels("Number of Clients", 50.0);
+		AxisLabels xAxis3 = AxisLabelsFactory.newAxisLabels("Throughput", 50.0);
 
 
-    // Defining chart.
-    LineChart chart = GCharts.newLineChart(line1, line2);
-    chart.setSize(600, 450);
-    chart.setTitle("Web Traffic|(in billions of hits)", Color.WHITE, 14);
-    chart.addHorizontalRangeMarker(40, 60, Color.newColor(Color.RED, 30));
-    chart.addVerticalRangeMarker(70, 90, Color.newColor(Color.GREEN, 30));
-    chart.setGrid(25, 25, 3, 2);
 
-    // Defining axis info and styles
-    AxisStyle axisStyle = AxisStyle.newAxisStyle(Color.WHITE, 12, AxisTextAlignment.CENTER);
-    AxisLabels xAxis = AxisLabelsFactory.newAxisLabels("Nov", "Dec", "Jan", "Feb", "Mar");
-    xAxis.setAxisStyle(axisStyle);
-    AxisLabels xAxis2 = AxisLabelsFactory.newAxisLabels("2007", "2007", "2008", "2008", "2008");
-    xAxis2.setAxisStyle(axisStyle);
-    AxisLabels yAxis = AxisLabelsFactory.newAxisLabels("", "25", "50", "75", "100");
-    AxisLabels xAxis3 = AxisLabelsFactory.newAxisLabels("Month", 50.0);
-    xAxis3.setAxisStyle(AxisStyle.newAxisStyle(Color.WHITE, 14, AxisTextAlignment.CENTER));
-    yAxis.setAxisStyle(axisStyle);
-    AxisLabels yAxis2 = AxisLabelsFactory.newAxisLabels("Hits", 50.0);
-    yAxis2.setAxisStyle(AxisStyle.newAxisStyle(Color.WHITE, 14, AxisTextAlignment.CENTER));
-    yAxis2.setAxisStyle(axisStyle);
+		// Adding axis info to chart.
+		chart.addXAxisLabels(xAxis);
+		chart.addYAxisLabels(yAxis);
+		chart.addYAxisLabels(yAxis2);
+		chart.addXAxisLabels(xAxis3);
 
-    // Adding axis info to chart.
-    chart.addXAxisLabels(xAxis);
-    chart.addXAxisLabels(xAxis2);
-    chart.addXAxisLabels(xAxis3);
-    chart.addYAxisLabels(yAxis);
-    chart.addYAxisLabels(yAxis2);
+		// Defining background and chart fills.
+		chart.setBackgroundFill(Fills.newSolidFill(Color.newColor("1F1D1D")));
+		LinearGradientFill fill = Fills.newLinearGradientFill(0, Color.newColor("363433"), 100);
+		fill.addColorAndOffset(Color.newColor("2E2B2A"), 0);
+		chart.setAreaFill(fill);
+		String url = chart.toURLString();
 
-    // Defining background and chart fills.
-    chart.setBackgroundFill(Fills.newSolidFill(Color.newColor("1F1D1D")));
-    LinearGradientFill fill = Fills.newLinearGradientFill(0, Color.newColor("363433"), 100);
-    fill.addColorAndOffset(Color.newColor("2E2B2A"), 0);
-    chart.setAreaFill(fill);
-    String url = chart.toURLString();
+		System.out.println(url);
+		return url;
+	}
 
-    return url;
-  }
+	private void saveChart(String url, String fileName) throws MalformedURLException, IOException {
+		URL u = new URL(url);
+		URLConnection uc = u.openConnection();
+		String contentType = uc.getContentType();
+		int contentLength = uc.getContentLength();
+		if (contentType.startsWith("text/") || contentLength == -1) {
+			throw new IOException("This is not a binary file.");
+		}
+		InputStream raw = uc.getInputStream();
+		InputStream in = new BufferedInputStream(raw);
+		byte[] data = new byte[contentLength];
+		int bytesRead = 0;
+		int offset = 0;
+		while (offset < contentLength) {
+			bytesRead = in.read(data, offset, data.length - offset);
+			if (bytesRead == -1) {
+				break;
+			}
+			offset += bytesRead;
+		}
+		in.close();
 
-  private void saveChart(String url, String fileName) throws MalformedURLException, IOException {
-    URL u = new URL(url);
-    URLConnection uc = u.openConnection();
-    String contentType = uc.getContentType();
-    int contentLength = uc.getContentLength();
-    if (contentType.startsWith("text/") || contentLength == -1) {
-      throw new IOException("This is not a binary file.");
-    }
-    InputStream raw = uc.getInputStream();
-    InputStream in = new BufferedInputStream(raw);
-    byte[] data = new byte[contentLength];
-    int bytesRead = 0;
-    int offset = 0;
-    while (offset < contentLength) {
-      bytesRead = in.read(data, offset, data.length - offset);
-      if (bytesRead == -1)
-        break;
-      offset += bytesRead;
-    }
-    in.close();
+		if (offset != contentLength) {
+			throw new IOException("Only read " + offset + " bytes; Expected " + contentLength + " bytes");
+		}
 
-    if (offset != contentLength) {
-      throw new IOException("Only read " + offset + " bytes; Expected " + contentLength + " bytes");
-    }
+		FileOutputStream out = new FileOutputStream(fileName);
+		out.write(data);
+		out.flush();
+		out.close();
 
-    FileOutputStream out = new FileOutputStream(fileName);
-    out.write(data);
-    out.flush();
-    out.close();
-
-  }
+	}
 }
