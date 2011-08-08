74aa7f987d0a295b93841273fd59cef60c2f6065
Piumi Nanayakkara <piumi.tiki@gmail.com>
2011-08-08 14:56:03 +0530

    removing global variables

--- FaultHandler/src/main/java/org/esbhive/fault_handler/FaultHandler.java
+++ FaultHandler/src/main/java/org/esbhive/fault_handler/FaultHandler.java
@@ -4,15 +4,18 @@ import java.rmi.RemoteException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.axis2.AxisFault;
+import org.apache.axis2.client.Options;
+import org.apache.axis2.client.ServiceClient;
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ConfigurationContextFactory;
 import org.esbhive.hp.mgt.ProxyAdminException;
+import org.esbhive.login.client.AuthenticationExceptionException;
 import org.wso2.carbon.utils.ServerConstants;
-import org.esbhive.node.mgt.xsd.ESBNode;
-
-//import org.esbhive.hp.mgt.*;
+import org.esbhive.login.*;
 import org.esbhive.hp.mgt.HiveProxyServiceAdminStub;
 import org.esbhive.proxyconf.mgt.ProxyConfManagerStub;
+import org.esbhive.proxyconf.mgt.xsd.ProxyDataList;
+import org.wso2.carbon.proxyadmin.xsd.ProxyData;
 
 /**
  *
@@ -22,17 +25,26 @@ import org.esbhive.proxyconf.mgt.ProxyConfManagerStub;
 /**
  * @scr.component name="Fault_Handler" immediate="true"
  * @scr.service interface="org.esbhive.fault_handler.FaultHandlerInterface"
+ * @scr.reference name="esbhive.login.service" interface="org.esbhive.login.RemoteLogin"
+ * cardinality="1..1" policy="dynamic" bind="setRemoteLogin"  unbind="unSetRemoteLogin"
+
  */
 public class FaultHandler implements FaultHandlerInterface {
 
     private String failedIp;
     private String failedPort;
     private boolean isFixed = false;
-    private ESBNode node;
-    private org.wso2.carbon.proxyadmin.xsd.ProxyData[] pdList;
-    HiveProxyServiceAdminStub hiveStub ;
-    ProxyConfManagerStub confStub;
-    String test = null;
+    private static HiveProxyServiceAdminStub hiveStub;
+    private static ProxyConfManagerStub confStub;
+    private static RemoteLogin remoteLogin;
+
+    protected void setRemoteLogin(RemoteLogin rl) {
+        remoteLogin = rl;
+    }
+
+    protected void unSetRemoteLogin(RemoteLogin rl) {
+        remoteLogin = null;
+    }
 
     public void setFailedNode(String ipAddress, String port) {
         this.failedIp = ipAddress;
@@ -42,71 +54,83 @@ public class FaultHandler implements FaultHandlerInterface {
     public String fixNode(String ipAddress, String port) {
         this.createStub();
         this.setFailedNode(ipAddress, port);
-        this.getProxyList();
-//        this.deleteProxyServices();
-//        this.redeployProxyServices();
+        ProxyData[] proxyDataList = this.getProxyList();
+        if (proxyDataList != null && proxyDataList.length != 0) {
+            this.deleteProxyServices(proxyDataList);
+            this.redeployProxyServices(proxyDataList);
+        }
         String result = this.getResult();
         this.reset();
         return result;
     }
 
-    public void getProxyList() {
+    public ProxyData[] getProxyList() {
+        ProxyData[] pdList = null;
+        ProxyDataList pdList0;
         try {
-            int j = 0;
             String ipandport = this.failedIp + ":" + this.failedPort;
-            org.esbhive.proxyconf.mgt.xsd.ProEsb[] proesb = confStub.getlist();
-            this.pdList = new org.wso2.carbon.proxyadmin.xsd.ProxyData[proesb.length];
-            for (int i=0; i < proesb.length; i++) {
-                if (proesb[i].getESBNode().getIpAndPort().equals(ipandport)) {
-                    test=test+proesb[i].getESBNode().getIpAndPort();
-                    this.pdList[j] = proesb[i].getProxyData();
-                }
+            pdList0 = confStub.getProxyDataList(ipandport);
+            if (pdList0 != null) {
+                pdList = pdList0.getProxyDataList();
             }
+
         } catch (RemoteException ex) {
             Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
-        this.isFixed = true;
+
+        return pdList;
 
     }
 
-    public void deleteProxyServices() {
-        try {
+    public void deleteProxyServices(ProxyData[] pdList) {
+        for (int i = 0; i < pdList.length; i++) {
+            try {
+                hiveStub.deleteProxyService(pdList[i].getName());
+            } catch (RemoteException ex) {
+                Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ProxyAdminException ex) {
+                Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 
-            hiveStub.deleteProxyService("StockQuoteProxy");
-            this.isFixed = true;
-            test="deleted";
-//           if (pdList != null) {
-//                for (int i = 0; i < pdList.length; i++) {
-//                    String proxyName = pdList[i].getName();
-//                    // hiveStub._deleteProxyService(proxyName);
-//                }
-//            }
-        } catch (ProxyAdminException ex) {
-            Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (RemoteException ex) {
-            Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
-        } 
     }
 
-    public void redeployProxyServices() {
-//        if (pdList != null) {
-//            for (int i = 0; i < pdList.length; i++) {
-//                // hiveStub._addProxy(pdList[i]);
-//            }
-//        }
-////        this.isFixed = true;
+    public void redeployProxyServices(ProxyData[] pdList) {
+
+        for (int i = 0; i < pdList.length; i++) {
+            try {
+                hiveStub.addProxy(pdList[i]);
+                this.isFixed = true;
+            } catch (RemoteException ex) {
+                Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ProxyAdminException ex) {
+                Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     private void reset() {
         this.failedIp = null;
         this.failedPort = null;
-
     }
 
     private void createStub() {
         try {
             String port = System.getProperty("carbon.https.port");
             String ipAddress = System.getProperty(ServerConstants.LOCAL_IP_ADDRESS);
+            LoginData otherNode = new LoginData();
+            otherNode.setUserName("admin");
+            otherNode.setPassWord("admin");
+            otherNode.setHostNameAndPort(ipAddress + ":" + port);
+            LoginData loginData = null;
+            try {
+                loginData = remoteLogin.logIn(otherNode);
+            } catch (RemoteException ex) {
+                Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (AuthenticationExceptionException ex) {
+                Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
             ConfigurationContext ctx = null;
             try {
                 ctx = ConfigurationContextFactory.createConfigurationContextFromFileSystem(null, null);
@@ -115,22 +139,38 @@ public class FaultHandler implements FaultHandlerInterface {
             }
             String serviceEPR1 = "https://" + ipAddress + ":" + port + "/services/" + "HiveProxyServiceAdmin";
             String serviceEPR2 = "https://" + ipAddress + ":" + port + "/services/" + "ProxyConfManager";
+
+
             hiveStub = new HiveProxyServiceAdminStub(ctx, serviceEPR1);
             confStub = new ProxyConfManagerStub(ctx, serviceEPR2);
+
+            ServiceClient proConfManagerClient = confStub._getServiceClient();
+            Options option = proConfManagerClient.getOptions();
+            option.setManageSession(true);
+            option.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING,
+                    loginData.getCookie());
+
+            ServiceClient hiveProxyAdminClient = hiveStub._getServiceClient();
+            Options option1 = hiveProxyAdminClient.getOptions();
+            option1.setManageSession(true);
+            option1.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING,
+                    loginData.getCookie());
+
         } catch (AxisFault ex) {
             Logger.getLogger(FaultHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 
     private String getResult() {
         String result = this.failedIp + ":" + this.failedPort;
         if (isFixed) {
             result = result + " was fixed sucessfully";
-            result=test;
         } else {
             result = result + " could not be fixed";
         }
+
         return result;
+
     }
 }
