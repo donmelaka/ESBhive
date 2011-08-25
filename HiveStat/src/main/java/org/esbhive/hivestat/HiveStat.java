2d45fe1abc276733de17a3363fa35d316404edb8
Melaka Gunasekara <donmelaka@gmail.com>
2011-08-26 00:20:21 +0530

    avoid null pointer error when accessing nodelist

--- HiveStat/src/main/java/org/esbhive/hivestat/HiveStat.java
+++ HiveStat/src/main/java/org/esbhive/hivestat/HiveStat.java
@@ -30,154 +30,165 @@ import org.wso2.carbon.proxyadmin.xsd.ProxyData;
  */
 public class HiveStat implements HiveStatInterface {
 
-	private static final Log log = LogFactory.getLog("org.esbhive.HiveStat");
-	private static RemoteLogin remoteLogin;
-	private NodeManagerInterface nodeManager;
+    private static final Log log = LogFactory.getLog("org.esbhive.HiveStat");
+    private static RemoteLogin remoteLogin;
+    private NodeManagerInterface nodeManager;
 
-	protected void setRemoteLogin(RemoteLogin rl) {
-		remoteLogin = rl;
-	}
+    protected void setRemoteLogin(RemoteLogin rl) {
+        remoteLogin = rl;
+    }
 
-	protected void unSetRemoteLogin(RemoteLogin rl) {
-		remoteLogin = null;
-	}
+    protected void unSetRemoteLogin(RemoteLogin rl) {
+        remoteLogin = null;
+    }
 
-	public synchronized void setNodeManager(NodeManagerInterface r) {
-		nodeManager = r;
-	}
+    public synchronized void setNodeManager(NodeManagerInterface r) {
+        nodeManager = r;
+    }
 
-	public synchronized void unsetNodeManager(NodeManagerInterface r) {
-		nodeManager = null;
-	}
+    public synchronized void unsetNodeManager(NodeManagerInterface r) {
+        nodeManager = null;
+    }
 
-	public String giveStat(String name) {
-		return "melaka";
-	}
+    public String giveStat(String name) {
+        return "melaka";
+    }
 
-	private ProxyConfManagerStub CreateProxyServiceAdminStub(String username, String password, String ipAndPort) {
+    private ProxyConfManagerStub CreateProxyServiceAdminStub(String username, String password, String ipAndPort) {
 
-		LoginData otherNode = new LoginData();
-		otherNode.setUserName(username);
-		otherNode.setPassWord(password);
-		otherNode.setHostNameAndPort(ipAndPort);
-		LoginData loginData = null;
-		try {
-			loginData = remoteLogin.logIn(otherNode);
-		} catch (AxisFault ex) {
-			log.error("AxisFault in HiveStat when login ", ex);
-		} catch (RemoteException ex) {
-			log.error("Remote exception in HiveStat when login ", ex);
-		} catch (org.esbhive.login.client.AuthenticationExceptionException ex) {
-			log.error("AuthenticationExceptionException in HiveStat when login ", ex);
-		}
-
-
-		ConfigurationContext ctx = null;
-		try {
-			ctx = ConfigurationContextFactory.createConfigurationContextFromFileSystem(null, null);
-		} catch (AxisFault ex) {
-			log.error("Axis fault in HiveStat ", ex);
-		}
+        LoginData otherNode = new LoginData();
+        otherNode.setUserName(username);
+        otherNode.setPassWord(password);
+        otherNode.setHostNameAndPort(ipAndPort);
+        LoginData loginData = null;
+        try {
+            loginData = remoteLogin.logIn(otherNode);
+        } catch (AxisFault ex) {
+            log.error("AxisFault in HiveStat when login ", ex);
+        } catch (RemoteException ex) {
+            log.error("Remote exception in HiveStat when login ", ex);
+        } catch (org.esbhive.login.client.AuthenticationExceptionException ex) {
+            log.error("AuthenticationExceptionException in HiveStat when login ", ex);
+        }
 
 
-		String serviceEPR4 = "https://" + ipAndPort + "/services/" + "ProxyConfManager";
-
-		ProxyConfManagerStub stub4 = null;
-		try {
-			stub4 = new ProxyConfManagerStub(ctx, serviceEPR4);
-		} catch (AxisFault ex) {
-			log.error("error while creating a new ProxyConfManager stub ", ex);
-		}
-		ServiceClient client4 = stub4._getServiceClient();
-		Options option = client4.getOptions();
-		option.setManageSession(true);
-		option.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, loginData.getCookie());
-		return stub4;
-	}
-
-	private int getCurrentLength(ESBNode array[], int j, ProxyConfManagerStub proxyConf) {
-		ProxyDataList proxyDataListObj = null;
-		ProxyData[] proxyDataList = null;
-		int k = 0;
-		try {
-			proxyDataListObj = proxyConf.getProxyDataList(array[j - 1].getIpAndPort());
-		} catch (RemoteException ex) {
-			log.error("RemoteException in HiveStat", ex);
-		}
-		if (proxyDataListObj != null) {
-			proxyDataList = proxyDataListObj.getProxyDataList();
-			k = proxyDataList.length;
-		}
-		return k;
-	}
-
-	private ESBNode[] insertionSort(ESBNode array[], ProxyConfManagerStub proxyConf) {
-		try {
-			ProxyDataList proxyDataListObj = null;
-			ProxyData[] proxyDataList = null;
-			ProxyDataList proxyDataListObj2 = null;
-			ProxyData[] proxyDataList1 = null;
-			//int k = 0;
-			int k2 = 0;
-			for (int i = 1; i < array.length; i++) {
-				int j = i;
-				ESBNode B = array[i];
-
-				proxyDataListObj2 = proxyConf.getProxyDataList(B.getIpAndPort());
-				if (proxyDataListObj2 != null) {
-					proxyDataList1 = proxyDataListObj2.getProxyDataList();
-					k2 = proxyDataList1.length;
-				} else {
-				k2=0;
-				}
-				while ((j > 0) && (getCurrentLength(array, j, proxyConf) > k2)) {
-					array[j] = array[j - 1];
-					j--;
-				}
-				array[j] = B;
-			}
-
-		} catch (Exception ex) {
-			log.error("RemoteException in HiveStat", ex);
-		}
-
-		return array;
+        ConfigurationContext ctx = null;
+        try {
+            ctx = ConfigurationContextFactory.createConfigurationContextFromFileSystem(null, null);
+        } catch (AxisFault ex) {
+            log.error("Axis fault in HiveStat ", ex);
+        }
 
 
-	}
+        String serviceEPR4 = "https://" + ipAndPort + "/services/" + "ProxyConfManager";
 
-	public ESBNode[] selectBestNodes() {
-		ESBNode[] nodeList = new ESBNode[0];
-		ESBNode[] sortedNodeList = null;
-		ArrayList<ESBNode> list = new ArrayList<ESBNode>();
-		if (nodeManager != null) {
-			nodeList = nodeManager.getNodes();
+        ProxyConfManagerStub stub4 = null;
+        try {
+            stub4 = new ProxyConfManagerStub(ctx, serviceEPR4);
+        } catch (AxisFault ex) {
+            log.error("error while creating a new ProxyConfManager stub ", ex);
+        }
+        ServiceClient client4 = stub4._getServiceClient();
+        Options option = client4.getOptions();
+        option.setManageSession(true);
+        option.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, loginData.getCookie());
+        return stub4;
+    }
+
+    private int getCurrentLength(ESBNode array[], int j, ProxyConfManagerStub proxyConf) {
+        ProxyDataList proxyDataListObj = null;
+        ProxyData[] proxyDataList = null;
+        int k = 0;
+        try {
+            proxyDataListObj = proxyConf.getProxyDataList(array[j - 1].getIpAndPort());
+        } catch (RemoteException ex) {
+            log.error("RemoteException in HiveStat", ex);
+        }
+        if (proxyDataListObj != null) {
+            proxyDataList = proxyDataListObj.getProxyDataList();
+            //k = proxyDataList.length;
+
+            if (proxyDataList != null) {
+                k = proxyDataList.length;
+            }
+
+        }
+        return k;
+    }
+
+    private ESBNode[] insertionSort(ESBNode array[], ProxyConfManagerStub proxyConf) {
+        try {
+            ProxyDataList proxyDataListObj = null;
+            ProxyData[] proxyDataList = null;
+            ProxyDataList proxyDataListObj2 = null;
+            ProxyData[] proxyDataList1 = null;
+            //int k = 0;
+            int k2 = 0;
+            for (int i = 1; i < array.length; i++) {
+                int j = i;
+                ESBNode B = array[i];
+
+                proxyDataListObj2 = proxyConf.getProxyDataList(B.getIpAndPort());
+                if (proxyDataListObj2 != null) {
+                    proxyDataList1 = proxyDataListObj2.getProxyDataList();
+                    //k2 = proxyDataList1.length;
+                    if (proxyDataList1 != null) {
+                        k2 = proxyDataList1.length;
+                    } else {
+                        k2 = 0;
+                    }
+
+                } else {
+                    k2 = 0;
+                }
+                while ((j > 0) && (getCurrentLength(array, j, proxyConf) > k2)) {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+                array[j] = B;
+            }
+
+        } catch (Exception ex) {
+            log.error("RemoteException in HiveStat", ex);
+        }
+
+        return array;
+
+
+    }
+
+    public ESBNode[] selectBestNodes() {
+        ESBNode[] nodeList = new ESBNode[0];
+        ESBNode[] sortedNodeList = null;
+        ArrayList<ESBNode> list = new ArrayList<ESBNode>();
+        if (nodeManager != null) {
+            nodeList = nodeManager.getNodes();
 //			for (ESBNode node : nodeList) {
 //				list.add(this.setNewEsbNode(node));
 //			}
 
-			sortedNodeList = insertionSort(nodeList, CreateProxyServiceAdminStub(nodeList[0].getUsername(), nodeList[0].getPassword(), nodeList[0].getIpAndPort()));
-		} else {
-			log.error("Error:: NodeManager is not set ");
-		}
-		if (remoteLogin != null) {
-			//log2.error("Error:: RemoteLogin is not set");
-		} else {
-			log.error("Error:: RemoteLogin is not set");
-		}
+            sortedNodeList = insertionSort(nodeList, CreateProxyServiceAdminStub(nodeList[nodeList.length - 1].getUsername(), nodeList[nodeList.length - 1].getPassword(), nodeList[nodeList.length - 1].getIpAndPort()));
+        } else {
+            log.error("Error:: NodeManager is not set ");
+        }
+        if (remoteLogin != null) {
+            //log2.error("Error:: RemoteLogin is not set");
+        } else {
+            log.error("Error:: RemoteLogin is not set");
+        }
 //		sortedNodeList = list.toArray(nodeList);
-		return sortedNodeList;
+        return sortedNodeList;
 
-	}
+    }
 
-	private ESBNode setNewEsbNode(org.esbhive.node.mgt.ESBNode node) {
-		org.esbhive.node.mgt.ESBNode newesbnode = new org.esbhive.node.mgt.ESBNode();
-		newesbnode.setIpAndPort(node.getIpAndPort());
-		newesbnode.setPassword(node.getPassword());
-		newesbnode.setUsername(node.getUsername());
-		newesbnode.setHttpsPort(node.getHttpsPort());
-		newesbnode.setSynapsePort(node.getSynapsePort());
-		newesbnode.setIp(node.getIp());
-		return newesbnode;
-	}
+    private ESBNode setNewEsbNode(org.esbhive.node.mgt.ESBNode node) {
+        org.esbhive.node.mgt.ESBNode newesbnode = new org.esbhive.node.mgt.ESBNode();
+        newesbnode.setIpAndPort(node.getIpAndPort());
+        newesbnode.setPassword(node.getPassword());
+        newesbnode.setUsername(node.getUsername());
+        newesbnode.setHttpsPort(node.getHttpsPort());
+        newesbnode.setSynapsePort(node.getSynapsePort());
+        newesbnode.setIp(node.getIp());
+        return newesbnode;
+    }
 }
